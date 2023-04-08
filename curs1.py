54f6296172c59c39359aa7fcf286704bfb2928bb
Razvi595 <razvanandrei595@yahoo.com>
2023-04-08 13:45:23 +0300

    1

--- curs1.py
+++ curs1.py
@@ -1,1 +1,8 @@
+from decimal import Decimal
 print("Hello world")
+price = Decimal(49)
+name = 'Razvan'
+print(f'For only {price:.2f} dollars {name} have a nice day')
+print(name)
+
+#strings are immutable
